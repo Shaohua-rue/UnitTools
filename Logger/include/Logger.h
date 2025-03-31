86e75e726cbbbed7dbda73ae1f51a4094759d7c9
shaohua <2726313053@qq.com>
2025-03-31 20:08:08 +0800

    v2.0

--- Logger/include/Logger.h
+++ Logger/include/Logger.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <iostream>
 #include <string>
-
-#include "noncopyable.h"
+#include "rang.hpp"
 
 
 
@@ -57,13 +57,56 @@ enum LogLevel
     FATAL,  //core dump 信息
     DEBUG   //调试信息
 };
+/************************时间戳类**********************/
 
-//输出一个日志类
-class Logger : noncopyable
+class Timestamp
+{
+public:
+    Timestamp();
+    explicit Timestamp(int64_t microSecondsSinceEpoch);
+    static Timestamp now();
+    std::string toString() const;
+private:
+    int64_t microSecondsSinceEpoch_;
+};
+
+Timestamp::Timestamp():microSecondsSinceEpoch_(0)
+{
+}
+
+Timestamp::Timestamp(int64_t microSecondsSinceEpoch):microSecondsSinceEpoch_ (microSecondsSinceEpoch)
+{
+}
+ 
+Timestamp Timestamp:: now()
+{
+    return Timestamp(time(NULL));
+}
+std::string Timestamp::toString() const
+{
+    char buf[128] = {0};
+    tm *tm_time = localtime(&microSecondsSinceEpoch_);
+    snprintf(buf, 128, "%4d/%02d/%02d %02d:%02d:%02d",
+            tm_time->tm_year + 1900,
+            tm_time->tm_mon + 1,
+            tm_time->tm_mday,
+            tm_time->tm_hour,
+            tm_time->tm_min,
+            tm_time->tm_sec);
+    return buf;
+}
+
+/*********************************日志类**************************************/
+class Logger 
 {
 public:
     //获取日志唯一的实例对象
     static Logger & instance();
+    Logger() = default;
+    ~Logger() = default;
+
+    Logger(const Logger&) = delete;
+    Logger &operator = (const Logger &) = delete;
 
     //设置日志级别
     void setLogLevel(int level);
@@ -75,3 +118,61 @@ public:
 private:
     int logLevel_;
 };
+
+//获取日志的唯一实例对象，单例
+
+Logger &Logger::instance()
+{
+    static Logger logger;
+    return logger;
+}
+
+//设置日志级别
+void Logger::setLogLevel(int level)
+{
+    logLevel_ = level;
+}
+
+//写日志[级别信息] time ：msg
+void Logger::log(std::string msg)
+{
+   switch (logLevel_)
+   {
+   case INFO:
+       std::cout << rang::style::bold 
+       << rang::fg::green
+       << "[INFO ] "
+       <<Timestamp::now().toString() << " " 
+       <<msg <<rang::style::reset << std::endl;
+       break;
+   case ERROR:
+        std::cout << rang::style::bold
+        << rang::fg::yellow
+        << "[ERROR] "
+        <<Timestamp::now().toString()<< " " 
+        << msg <<rang::style::reset << std::endl;
+       break;
+   case FATAL:
+        std::cout << rang::style::bold
+        << rang::fg::red
+        << "[FATAL] "<<
+       Timestamp::now().toString() << " "  
+        << msg <<rang::style::reset << std::endl;
+       break;
+   case DEBUG:
+        std::cout << rang::style::bold << "[DEBUG] "
+       <<Timestamp::now().toString() << " " 
+       << msg <<rang::style::reset<< std::endl;
+       break;
+   default:
+       break;
+   }
+   
+  
+}
+
+
+
+
+
+
